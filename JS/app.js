467bdadbf2b349a2b44ddd8900edbeccebe24c1f
Unknown <unknownmagma008@gmail.com>
2025-08-12 12:26:52 +0500

    12 August 2025 Tuesday 12:26PM

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,34 @@
 const menu = document.getElementById("menu")
 const miniUl = document.querySelector(".miniUl")
+const dropClose = document.querySelectorAll(".dropClose")
 
-console.log(miniUl);
 
 menu.addEventListener("click", () => {
     miniUl.classList.toggle("addHeight")
     menu.classList.toggle("rotateX")
+})
+
+
+dropClose.forEach((item) => {
+    item.addEventListener("click", () => {
+        const dropDown = item.parentElement.parentElement.lastElementChild
+        item.classList.toggle("rotated")
+        if (dropDown.style.height === "" || dropDown.style.height === "0px") {
+            dropDown.style.height = dropDown.scrollHeight + "px"
+
+            dropDown.addEventListener("transitioned", 
+            function setAuto() {
+                dropDown.style.height = "auto"                
+            })
+
+            dropDown.removeEventListener("transitioned", 
+            function setAuto() {
+                dropDown.style.height = "auto"                
+            })
+        } else {
+            dropDown.style.height = dropDown.scrollHeight + "px"
+            dropDown.offsetHeight;
+            dropDown.style.height = "0px"
+        }
+    })
 })
